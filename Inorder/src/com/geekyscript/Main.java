c80a7b5dbed3bfebb115c1de072af5bc5bbd49cf
_isha_30 <choudhary.isha30@gmail.com>
2021-12-02 18:55:28 +0530

    Traversals

---
+++ Inorder/src/com/geekyscript/Main.java
@@ -0,0 +1,45 @@
+package com.geekyscript;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Node root = new Node(10);
+        root.left = new Node(20);
+        root.right = new Node(30);
+        root.left.left = new Node(40);
+        root.left.right = new Node(50);
+        root.right.right = new Node(60);
+
+        inorder(root);
+        System.out.println();
+        preorder(root);
+        System.out.println();
+        postorder(root);
+    }
+
+    public static void inorder(Node root){
+        if(root!=null){
+            inorder(root.left);
+            System.out.print(root.data+" ");
+            inorder(root.right);
+        }
+    }
+
+    public static void preorder(Node root){
+        if(root!=null){
+            System.out.print(root.data+" ");
+            preorder(root.left);
+            preorder(root.right);
+        }
+    }
+
+    public static void postorder(Node root){
+        if(root!=null){
+            postorder(root.left);
+            postorder(root.right);
+            System.out.print(root.data+" ");
+        }
+    }
+
+}
