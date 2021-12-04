c9e3be916e2ff531974c03f71d69d0ba305dbf8b
_isha_30 <choudhary.isha30@gmail.com>
2021-12-04 22:27:58 +0530

    No. of Nodes in Binary Tree

---
+++ getSize/src/com/geekyscript/Main.java
@@ -0,0 +1,19 @@
+package com.geekyscript;
+
+public class Main {
+    public static void main(String[] args) {
+        Node root = new Node(10);
+        root.left = new Node(20);
+        root.left.left = new Node(30);
+        root.left.right = new Node(40);
+        root.right = new Node(50);
+        root.right.left = new Node(60);
+
+        System.out.println(treeSize(root));
+
+    }
+    public static int treeSize(Node root){
+        if(root==null) return 0;
+        else return (1+treeSize(root.left)+treeSize(root.right));
+    }
+}
