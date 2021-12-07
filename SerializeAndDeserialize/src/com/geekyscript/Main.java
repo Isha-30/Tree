d570f7fced288c3c00810b84f2e92e1939eab8fa
_isha_30 <choudhary.isha30@gmail.com>
2021-12-07 14:47:34 +0530

    Serialize and deserialize binary tree

---
+++ SerializeAndDeserialize/src/com/geekyscript/Main.java
@@ -0,0 +1,53 @@
+package com.geekyscript;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String args[])
+    {
+        Node root=new Node(10);
+        root.left=new Node(20);
+
+        ArrayList<Integer> arr=new ArrayList<>();
+        serialize(root,arr);
+        for (int x : arr) {
+            System.out.print(x+" ");
+        }
+        System.out.println();;
+        Node root_new=deSerialize(arr);
+        inorder(root_new);
+
+    }
+
+    static final int EMPTY=-1;
+    public static void serialize(Node root, ArrayList<Integer> arr){
+        if(root==null){
+            arr.add(EMPTY);
+            return;
+        }
+        arr.add(root.data);
+        serialize(root.left,arr);
+        serialize(root.right,arr);
+    }
+
+    static int index=0;
+    public static Node deSerialize(ArrayList<Integer> arr){
+        if(index==arr.size())
+            return null;
+        int val=arr.get(index);
+        index++;
+        if(val==EMPTY)return null;
+        Node root=new Node(val);
+        root.left=deSerialize(arr);
+        root.right=deSerialize(arr);
+        return root;
+    }
+
+    public static void inorder(Node root){
+        if(root!=null){
+            inorder(root.left);
+            System.out.print(root.data+" ");
+            inorder(root.right);
+        }
+    }
+}
